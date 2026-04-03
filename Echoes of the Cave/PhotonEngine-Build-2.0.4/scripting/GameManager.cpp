fd9b66f3abfe365111ff8111bc6e9807fad087c1
Lea Pavel <leapavel602@gmail.com>
2026-04-03 15:27:12 +0200

    Add / beginning of the GameManager

---
+++ Echoes of the Cave/PhotonEngine-Build-2.0.4/scripting/GameManager.cpp
@@ -0,0 +1,92 @@
+#include "script_pch.h"
+
+#ifdef _WIN32
+#define SCRIPT_API __declspec(dllexport)
+#else
+#define SCRIPT_API __attribute__((visibility("default")))
+#endif
+
+class GameManager : public Engine::Scripting::NativeScript {
+private:
+	int puzzlesSolved = 0;
+	bool gameWon = false;
+
+	std::string puzzle1Door = "Door_Puzzle1";
+	std::string puzzle2Door = "Door_Puzzle2";
+	std::string puzzle3Door = "Door_Puzzle3";
+
+public:
+	int totalPuzzles = 3;
+
+	std::string doorEntityName = "FinalDoor";
+	std::string treasureEntityName = "Treasure";
+
+private:
+	void OnPuzzleSolved(int puzzleID) {
+		puzzlesSolved++;
+
+		if (TerminalInstance)
+			TerminalInstance->info("GameManager: Puzzle " + std::to_string(puzzleID) +
+				" résolu ! (" + std::to_string(puzzlesSolved) + "/" +
+				std::to_string(totalPuzzles) + ")");
+
+		OpenDoor(puzzleID);
+		
+		if (puzzlesSolved >= totalPuzzles) {
+			WinGame();
+		}
+	}
+
+	void OpenDoor(int puzzleID) {
+		std::string doorName;
+		if (puzzleID == 1) { doorName = puzzle1Door; }
+		else if (puzzleID == 2) { doorName = puzzle2Door; }
+		else if (puzzleID == 3) { doorName = puzzle3Door; }
+		else return;
+
+		for (auto e : registry->View<Engine::Components::Transform>()) {
+			if (registry->GetEntityName(e) == doorName) {
+				auto& t = registry->GetComponent<Engine::Components::Transform>(e); 
+				t.Position.y -= 3.0f;
+				if (TerminalInstance)
+					TerminalInstance->info("GameManager: Porte '" + doorName + "' ouverte !");
+				break;
+			}
+		}
+	}
+
+	void WinGame() {
+		gameWon = true;
+		if (TerminalInstance)
+			TerminalInstance->info("GameManager: VICTOIRE !");
+
+		for (auto e : registry->View<Engine::Components::Transform>()) {
+			if (registry->GetEntityName(e) == doorEntityName) {
+				auto& t = registry->GetComponent<Engine::Components::Transform>(e);
+				t.Position.y -= 5.f;
+				if (TerminalInstance)
+					TerminalInstance->info("GameManager: Porte finale ouverte!");
+				break;
+			}
+		}
+	}
+public:
+	void OnInit() override {
+		Inspect("Total Puzzles", &totalPuzzles);
+	}
+	void OnCreate() override {
+
+	}
+
+	void OnUpdate(float deltaTime) override {
+
+	}
+
+	void OnDestroy() override {
+
+	}
+};
+
+extern "C" SCRIPT_API Engine::Scripting::NativeScript* CreateScript() {
+	return new FPSCamera();
+}
