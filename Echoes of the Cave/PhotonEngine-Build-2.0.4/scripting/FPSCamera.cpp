2b08126b83f6c4a41dbaaaaa762a716c5e01e286
LiamHanson971 <lhanson@gaming.tech>
2026-04-07 18:00:57 +0200

    Fix / cam player pas fix j'arrive pas

--- Echoes of the Cave/PhotonEngine-Build-2.0.4/scripting/FPSCamera.cpp
+++ Echoes of the Cave/PhotonEngine-Build-2.0.4/scripting/FPSCamera.cpp
@@ -111,18 +111,8 @@ public:
         glm::vec3 inputDirection(0.0f);
 
         if (isMouseCaptured) {
-            if (InputSysteminstance->GetKeyState(GLFW_KEY_W)) {
-                player.Position += flatForward * moveSpeed * dt;
-
-                player.Rotation.y = yaw + 180.0f;
-            }
-
-            if (InputSysteminstance->GetKeyState(GLFW_KEY_S)) {
-                player.Position -= flatForward * moveSpeed * dt;
-
-                player.Rotation.y = yaw + 180.0f;
-            }
-
+            if (InputSysteminstance->GetKeyState(GLFW_KEY_W)) player.Position += flatForward * moveSpeed * dt;
+            if (InputSysteminstance->GetKeyState(GLFW_KEY_S)) player.Position -= flatForward * moveSpeed * dt;
             if (InputSysteminstance->GetKeyState(GLFW_KEY_D)) player.Position += flatRight * moveSpeed * dt;
             if (InputSysteminstance->GetKeyState(GLFW_KEY_A)) player.Position -= flatRight * moveSpeed * dt;
         }
@@ -142,6 +132,10 @@ public:
         if (glm::length(inputDirection) > 0.0f) {
             inputDirection = glm::normalize(inputDirection) * currentSpeed;
         }
+
+        player.Rotation.y = yaw + 180.0f;
+        cam.Position = player.Position + glm::vec3(0.f, 0.07f, -0.05f);
+
         if (physicsSystem) {
             glm::vec3 currentVel = physicsSystem->GetLinearVelocity(targetEntity);
             auto& t = registry->GetComponent<Engine::Components::Transform>(targetEntity);
@@ -205,57 +199,8 @@ public:
                 
                 
             }
-
-            player.Rotation.y = yaw + 180.0f;
-
-            cam.Position = player.Position + glm::vec3(0.f, 0.07f, -0.05f);
+            
         }
-
-        //capturedEntities();
-        /* {
-
-            if (InputSysteminstance->GetMouseButtonPressed(0)) {
-                isButMousePressed = !isButMousePressed;
-                InputSysteminstance->SetMouseCapture(isButMousePressed);
-            }
-
-            // SEULEMENT si le bouton souris est maintenu
-            if (isButMousePressed)
-            {
-                //auto& cameraTransform = registry->GetComponent<Engine::Components::Transform>(targetEntity);
-                auto physicsSystem = engine->GetSystem<Engine::Systems::PhysicsSystem>();
-
-                // DIRECTION du regard de la caméra (normalement -Z ou Z selon ta convention)
-                //glm::vec3 cameraForward = glm::normalize(cameraTransform.GetForward()); // ou glm::vec3(0, 0, -1) si tu utilises une direction fixe
-                //glm::vec3 forward = glm::normalize(glm::vec3(sin(yaw) * cos(pitch), -sin(pitch), -cos(yaw) * cos(pitch)));
-
-                // Point de départ : position de la caméra
-                glm::vec3 rayOrigin = cam.Position;
-
-                // Point d'arrivée : un point loin devant (ex: 1000 unités)
-                glm::vec3 rayEnd = rayOrigin + cam.Forward * 1000.0f;
-
-                Engine::Systems::PhysicsUtils::RaycastHit hitResult = physicsSystem->Raycast(
-                    rayOrigin,           // Départ : caméra
-                    rayEnd,              // Fin : direction du regard
-                    targetEntity,
-                    { true, 0.1f, {1, 0, 0}, {1, 1, 0}, {0, 1, 1}, {0.5, 0.5, 0.5}, 0.05f, 0.012f }
-                );
-
-                std::string HitEntityName = registry->GetEntityName(hitResult.hitEntity);
-                TerminalInstance->print("RayCast Hit " + HitEntityName);
-
-                if (HitEntityName == "Cube") {
-                    entitietouch = true;
-                }
-                else
-                {
-                    entitietouch = false;
-                }
-            }
-
-        }
-        */
         
     }
 
